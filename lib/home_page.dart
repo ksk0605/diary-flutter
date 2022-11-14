997753ae858ce6e6b1d10197251985debc1b833f
kimsungkyum <sungkyum1@naver.com>
2022-11-14 17:31:16 +0900

    feat/메모구현

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:diary/diary_service.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -15,14 +16,19 @@ class _HomePageState extends State<HomePage> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
+  TextEditingController createTextController = TextEditingController();
+  TextEditingController updateTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<DiaryService>(
       builder: (context, diaryService, child) {
+        List<Diary> diaryList = diaryService.getByDate(_focusedDay);
+
         return Scaffold(
           floatingActionButton: FloatingActionButton(
             onPressed: () {
-              // Add your onPressed code here!
+              showCreateDialog(diaryService);
             },
             child: Icon(Icons.create),
             backgroundColor: Colors.indigo,
@@ -36,11 +42,6 @@ class _HomePageState extends State<HomePage> {
                   focusedDay: _focusedDay,
                   calendarFormat: _calendarFormat,
                   selectedDayPredicate: (day) {
-                    // Use `selectedDayPredicate` to determine which day is currently selected.
-                    // If this returns true, then `day` will be marked as selected.
-
-                    // Using `isSameDay` is recommended to disregard
-                    // the time-part of compared DateTime objects.
                     return isSameDay(_selectedDay, day);
                   },
                   onDaySelected: (selectedDay, focusedDay) {
@@ -68,9 +69,223 @@ class _HomePageState extends State<HomePage> {
                 Divider(
                   color: Colors.grey,
                 ),
+                Expanded(
+                  child: diaryList.isEmpty
+                      ? Center(
+                          child: Text(
+                            "한 줄 일기를 작성해주세요.",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 18,
+                            ),
+                          ),
+                        )
+                      : ListView.builder(
+                          itemCount: diaryList.length,
+                          itemBuilder: ((context, index) {
+                            int i = diaryList.length - index - 1;
+                            Diary diary = diaryList[i];
+                            return ListTile(
+                              title: Text(
+                                diary.text,
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              trailing: Text(
+                                DateFormat('kk:mm').format(diary.createdAt),
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              onTap: (() {
+                                showUpdateDialog(diaryService, diary);
+                              }),
+                              onLongPress: () {
+                                showDeleteDialog(diaryService, diary);
+                              },
+                            );
+                          }),
+                        ),
+                )
               ],
             ),
           ),
+        );
+      },
+    );
+  }
+
+  void createDiary(DiaryService diaryService) {
+    // 앞뒤 공백 삭제
+    String newText = createTextController.text.trim();
+    if (newText.isNotEmpty) {
+      diaryService.create(newText, _focusedDay);
+      createTextController.text = "";
+    }
+  }
+
+  void updateDiary(DiaryService diaryService, Diary diary) {
+    // 앞뒤 공백 삭제
+    String updatedText = updateTextController.text.trim();
+    if (updatedText.isNotEmpty) {
+      diaryService.update(
+        diary.createdAt,
+        updatedText,
+      );
+    }
+  }
+
+  /// 작성 다이얼로그 보여주기
+  void showCreateDialog(DiaryService diaryService) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("일기 작성"),
+          content: TextField(
+            controller: createTextController,
+            autofocus: true,
+            // 커서 색상
+            cursorColor: Colors.indigo,
+            decoration: InputDecoration(
+              hintText: "한 줄 일기를 작성해주세요.",
+              // 포커스 되었을 때 밑줄 색상
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.indigo),
+              ),
+            ),
+            onSubmitted: (_) {
+              // 엔터 누를 때 작성하기
+              createDiary(diaryService);
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            /// 취소 버튼
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text(
+                "취소",
+                style: TextStyle(color: Colors.indigo),
+              ),
+            ),
+
+            /// 작성 버튼
+            TextButton(
+              onPressed: () {
+                createDiary(diaryService);
+                Navigator.pop(context);
+              },
+              child: Text(
+                "작성",
+                style: TextStyle(color: Colors.indigo),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  /// 수정 다이얼로그 보여주기
+  void showUpdateDialog(DiaryService diaryService, Diary diary) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        updateTextController.text = diary.text;
+        return AlertDialog(
+          title: Text("일기 수정"),
+          content: TextField(
+            autofocus: true,
+            controller: updateTextController,
+            // 커서 색상
+            cursorColor: Colors.indigo,
+            decoration: InputDecoration(
+              hintText: "한 줄 일기를 작성해 주세요.",
+              // 포커스 되었을 때 밑줄 색상
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.indigo),
+              ),
+            ),
+            onSubmitted: (v) {
+              // 엔터 누를 때 수정하기
+              updateDiary(diaryService, diary);
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            /// 취소 버튼
+            TextButton(
+              child: Text(
+                "취소",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.indigo,
+                ),
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+
+            /// 수정 버튼
+            TextButton(
+              child: Text(
+                "수정",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.indigo,
+                ),
+              ),
+              onPressed: () {
+                // 수정하기
+                updateDiary(diaryService, diary);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  /// 삭제 다이얼로그 보여주기
+  void showDeleteDialog(DiaryService diaryService, Diary diary) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        updateTextController.text = diary.text;
+        return AlertDialog(
+          title: Text("일기 삭제"),
+          content: Text('"${diary.text}"를 삭제하시겠습니까?'),
+          actions: [
+            TextButton(
+              child: Text(
+                "취소",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.indigo,
+                ),
+              ),
+              onPressed: () => Navigator.pop(context),
+            ),
+
+            /// Delete
+            TextButton(
+              child: Text(
+                "삭제",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.indigo,
+                ),
+              ),
+              onPressed: () {
+                diaryService.delete(diary.createdAt);
+                Navigator.pop(context);
+              },
+            ),
+          ],
         );
       },
     );
